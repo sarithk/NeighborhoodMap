c7f60d98ef3a54b961149461795d85657bda5033
Saritha Kamath <sarithakamath24@gmail.com>
2017-11-26 15:22:01 -0800

    Initial Commit
    Basic version of neighborhood map. Google map is loaded.
    A side left panel if alloted for search functionality.

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 11,
+          center: {lat: 37.54827, lng: -121.988572}
+        });
+      }
