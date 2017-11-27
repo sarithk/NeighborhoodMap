dfcb57da5cb5f77ec3cd86fb6efcd6767ead6459
Saritha Kamath <sarithakamath24@gmail.com>
2017-11-27 13:01:16 -0800

    feat: Change to display markers when app loads
    Added initial locations, created map markers.
    Included Knockout JS to have a model and view model.
    Made style change to search panel and listings.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,40 @@
+//Model
+//List of initial locations to be dispalyed on map
+var initialLocations = [
+{title: 'Ardenwood Historic Farm', location:{lat:37.556817, lng:-122.048231}},
+{title: 'Lake Elizabeth Park', location:{lat:37.543634, lng:-121.962885}},
+{title: "Children's Discovery Museum", location:{lat:37.326755, lng:-121.89252}},
+{title: 'Niles Canyon Railway', location:{lat:37.578259, lng:-121.980858}},
+{title: "California's Great America", location:{lat:37.398421, lng:-121.975126}}
+];
+
+//View Model for the app
+var mapViewModel = function(map) {
+     //Display initial locations
+     //Create a new blank array for initial listing markers
+     var markers = [];
+
+     //Loop through the initial locations
+     for ( var i = 0; i < initialLocations.length; i ++ ) {
+     //Create a marker for every location
+        var marker = new google.maps.Marker({
+            map: map,
+            position: initialLocations[i].location,
+            title: initialLocations[i].title,
+            animation: google.maps.Animation.DROP,
+            id:i
+        });
+     //Push the marker into the markers array defined earlier
+       markers.push(marker);
+       console.log(markers.length,marker);
+   }
+    }
+
+
 function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 11,
+          zoom: 10,
           center: {lat: 37.54827, lng: -121.988572}
         });
-      }
+ko.applyBindings(new mapViewModel(map));
+}
